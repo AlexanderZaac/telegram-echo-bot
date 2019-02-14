6cc0c4ec7fc82a6ad5881a5c9a04ab3bb20427ad
Alessandro Zappia <36998598+alezap@users.noreply.github.com>
2019-02-14 23:54:47 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://notifier.herokuapp.com/execute.php';
+$BOT_TOKEN = '679423412:AAGvDET8eVRjzYWFMnlvXAfzoADN1-I64Dg';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
